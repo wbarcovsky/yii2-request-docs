751d5010b51ea35b8adc30078e16605047e30796
BaranovNV <BaranovNV@altarix.ru>
2018-07-11 18:06:32 +0400

    add showdown

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,4 @@
+var converter = new showdown.Converter();
 
 function toggleBox(box) {
   box.classList.toggle('box-close');
@@ -34,7 +35,7 @@ function loadParams(element, hash, tabClass) {
           showData = json.params;
           break;
         case 'description':
-          showData = json.description;
+          showData = converter.makeHtml(json.description);
           break;
     }
     if (Array.isArray(showData) && showData.length > 0) {
